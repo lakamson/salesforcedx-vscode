dc119c7cb520ac58124e22a094adb1acf5a268e6
Luis Campos <lcamposguajardo@salesforce.com>
2020-04-06 15:38:04 -0700

    Updates to use latest changes in the library's retrieve branch

--- packages/salesforcedx-vscode-core/src/commands/util/libraryCommandlet.ts
+++ packages/salesforcedx-vscode-core/src/commands/util/libraryCommandlet.ts
@@ -5,11 +5,15 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { Connection } from '@salesforce/core';
 import { ContinueResponse } from '@salesforce/salesforcedx-utils-vscode/out/src/types';
-import { ToolingRetrieveResult } from '@salesforce/source-deploy-retrieve/lib/tooling/retrieve';
+import {
+  SourceClient,
+  ToolingDeployResult
+} from '@salesforce/source-deploy-retrieve';
+import { ApiResult } from '@salesforce/source-deploy-retrieve/lib/client/client';
 import { ProgressLocation, window } from 'vscode';
 import { channelService } from '../../channels';
+import { ToolingDeployParser } from '../../deploys';
 import { nls } from '../../messages';
 import { notificationService } from '../../notifications';
 import { TelemetryData, telemetryService } from '../../telemetry';
@@ -19,7 +23,7 @@ import { CommandletExecutor } from './sfdxCommandlet';
 export abstract class LibraryCommandletExecutor<T>
   implements CommandletExecutor<T> {
   protected showChannelOutput = true;
-  protected orgConnection: Connection | undefined;
+  protected sourceClient: SourceClient | undefined;
   protected executionName: string = '';
   protected startTime: [number, number] | undefined;
   protected telemetryName: string | undefined;
@@ -37,15 +41,14 @@ export abstract class LibraryCommandletExecutor<T>
     if (!usernameOrAlias) {
       throw new Error(nls.localize('error_no_default_username'));
     }
-    this.orgConnection = await OrgAuthInfo.getConnection(usernameOrAlias);
+    const conn = await OrgAuthInfo.getConnection(usernameOrAlias);
+    this.sourceClient = new SourceClient(conn, '48.0');
   }
 
-  public retrieveWrapper(
-    fn: (...args: any[]) => Promise<ToolingRetrieveResult>
-  ) {
+  public retrieveWrapper(fn: (...args: any[]) => Promise<ApiResult>) {
     const commandName = this.executionName;
 
-    return async function(...args: any[]): Promise<ToolingRetrieveResult> {
+    return async function(...args: any[]): Promise<ApiResult> {
       channelService.showCommandWithTimestamp(`Starting ${commandName}`);
 
       const result = await window.withProgress(
@@ -55,11 +58,37 @@ export abstract class LibraryCommandletExecutor<T>
         },
         async () => {
           // @ts-ignore
-          return (await fn.call(this, ...args)) as ToolingRetrieveResult;
+          return (await fn.call(this, ...args)) as ApiResult;
         }
       );
 
       channelService.appendLine('Library result =>' + JSON.stringify(result));
+      channelService.showCommandWithTimestamp(`Finished ${commandName}`);
+      await notificationService.showSuccessfulExecution(commandName);
+      return result;
+    };
+  }
+
+  public deployWrapper(fn: (...args: any[]) => Promise<ToolingDeployResult>) {
+    const commandName = this.executionName;
+
+    return async function(...args: any[]): Promise<ToolingDeployResult> {
+      channelService.showCommandWithTimestamp(`Starting ${commandName}`);
+
+      const result = await window.withProgress(
+        {
+          title: commandName,
+          location: ProgressLocation.Notification
+        },
+        async () => {
+          // @ts-ignore
+          return (await fn.call(this, ...args)) as ToolingDeployResult;
+        }
+      );
+
+      const parser = new ToolingDeployParser(result);
+      const outputResult = await parser.outputResult();
+      channelService.appendLine(outputResult);
       channelService.showCommandWithTimestamp(`Finished ${commandName}`);
       await notificationService.showSuccessfulExecution(commandName);
       return result;
